dcb82c5210fa460d524e69d330a5c29d80e60b54
perfaram <perfaram@windowslive.com>
2014-10-21 18:22:39 +0200

    Corrected mistake

--- src/ins_classic.h
+++ src/ins_classic.h
@@ -34,6 +34,6 @@
     "Do you think like you type?",
     "Your mind just hasn't been the same since the electro-shock, has it?",
     "The problem lies between the keyboard and the chair.",
-    '"Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning."'
+    "\"Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.\"",
 
 #endif /* _SUDO_INS_CLASSIC_H */
